e72cc36b69e9fb9d6024685c800e7f778ce8e9b2
Choegyel <mac@Chogyal.local>
2025-01-26 20:33:44 +0600

    first commit

---
+++ src/main/java/com/personalAssist/MindMap/MindMapApplication.java
@@ -0,0 +1,13 @@
+package com.personalAssist.MindMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personalAssist/MindMap/MindMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalAssist.MindMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
